e9b878d02618e149491aa977206674d63ee08a04
haziqshifa2112 <160044831+haziqshifa2112@users.noreply.github.com>
2024-06-10 01:12:31 +0800

    Add files via upload

---
+++ Connection.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Data.OleDb
+
+Module Connection
+    Public ds As New DataSet
+    Public cmd As New OleDbCommand
+    Public conn As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\User\Desktop\RevisionVB\RevisionVB.mdb")
+    Public dr As OleDbDataReader
+    Public dt As DataTable
+End Module
